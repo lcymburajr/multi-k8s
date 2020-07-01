6ee54493ce3ca19ca69082eb67bc1ea09585a07c
Lawrence Cymbura <lcymburajr@ljcdesign.com>
2020-07-01 10:56:55 -0400

    fix typo

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t $DOCKER_ID/multi-client:latest -t $DOCKER_ID/multi-client:$SHA ./client/Dockerfile ./client
+docker build -t $DOCKER_ID/multi-server:latest -t $DOCKER_ID/multi-server:$SHA ./server/Dockerfile /server
+docker build -t $DOCKER_ID/multi-worker:latest -t $DOCKER_ID/multi-worker:$SHA ./worker/Dockerfile ./worker
+
+docker push $DOCKER_ID/multi-client:latest
+docker push $DOCKER_ID/multi-server:latest
+docker push $DOCKER_ID/multi-worker:latest
+
+docker push $DOCKER_ID/multi-client:$SHA
+docker push $DOCKER_ID/multi-server:$SHA
+docker push $DOCKER_ID/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployment/client-deployment client=$DOCKER_ID/multi-client:$SHA
+kubectl set image deployment/server-deployment server=$DOCKER_ID/multi-server:$SHA
+kubectl set image deployment/worker-deployment worker=$DOCKER_ID/multi-worker:$SHA
