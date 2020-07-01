ecf48205a611991fdf12b16c485720a9558279d4
Lawrence Cymbura <lcymburajr@ljcdesign.com>
2020-07-01 10:55:04 -0400

    update script

--- depoy.sh
+++ depoy.sh
@@ -1,16 +1,16 @@
-docker build -t $DOCKER_ID/multi-client:latest -t $DOCKER_ID/multi-client:$SHA ./client/Dockerfile ./client
-docker build -t $DOCKER_ID/multi-server:latest -t $DOCKER_ID/multi-server:$SHA ./server/Dockerfile /server
-docker build -t $DOCKER_ID/multi-worker:latest -t $DOCKER_ID/multi-worker:$SHA ./worker/Dockerfile ./worker
+docker build -t lcymburajr/multi-client:latest -t lcymburajr/multi-client:$SHA ./client/Dockerfile ./client
+docker build -t lcymburajr/multi-server:latest -t lcymburajr/multi-server:$SHA ./server/Dockerfile /server
+docker build -t lcymburajr/multi-worker:latest -t lcymburajr/multi-worker:$SHA ./worker/Dockerfile ./worker
 
-docker push $DOCKER_ID/multi-client:latest
-docker push $DOCKER_ID/multi-server:latest
-docker push $DOCKER_ID/multi-worker:latest
+docker push lcymburajr/multi-client:latest
+docker push lcymburajr/multi-server:latest
+docker push lcymburajr/multi-worker:latest
 
-docker push $DOCKER_ID/multi-client:$SHA
-docker push $DOCKER_ID/multi-server:$SHA
-docker push $DOCKER_ID/multi-worker:$SHA
+docker push lcymburajr/multi-client:$SHA
+docker push lcymburajr/multi-server:$SHA
+docker push lcymburajr/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployment/client-deployment client=$DOCKER_ID/multi-client:$SHA
-kubectl set image deployment/server-deployment server=$DOCKER_ID/multi-server:$SHA
-kubectl set image deployment/worker-deployment worker=$DOCKER_ID/multi-worker:$SHA
+kubectl set image deployment/client-deployment client=lcymburajr/multi-client:$SHA
+kubectl set image deployment/server-deployment server=lcymburajr/multi-server:$SHA
+kubectl set image deployment/worker-deployment worker=lcymburajr/multi-worker:$SHA
